5279bf339132cb9fa26f12ebfc646a31081cd00c
Yonghao-Yu <ieltser@163.com>
2024-04-24 18:47:30 +0800

    改变目录结构

--- main.py
+++ main.py
@@ -208,7 +208,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('training and evaluation script', parents=[get_args_parser()])
     args = parser.parse_args()
-    args.output_dir = args.model + '_' + args.opt + '_' + str(args.lr)
+    args.output_dir = args.model + '/' + args.opt + '/' + str(args.lr)
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
     main(args)
